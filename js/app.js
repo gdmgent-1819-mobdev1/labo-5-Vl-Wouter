6005cb16bf1bf804a2228a7dd0cd6813d09124e4
Wouter Vlaeyen <36659001+Vl-Wouter@users.noreply.github.com>
2018-10-24 21:20:48 +0200

    First pages taking shape

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBlGyQkZwwX_WBoifrJaOlLhMjt-In4P2E",
+    authDomain: "mobdev-1-labo5.firebaseapp.com",
+    databaseURL: "https://mobdev-1-labo5.firebaseio.com",
+    projectId: "mobdev-1-labo5",
+    storageBucket: "mobdev-1-labo5.appspot.com",
+    messagingSenderId: "667824028304"
+};
+firebase.initializeApp(config);
